3c97a885a2262d78411ed2154b117924819b2eba
rportocarrero <rportocarrero@gmail.com>
2019-11-10 18:14:12 -0500

    Github

---
+++ README.md
@@ -0,0 +1,7 @@
+# &lt;cells-bitcoin-dm&gt;
+
+![Polymer 2.x](https://img.shields.io/badge/Polymer-2.x-green.svg)
+Example:
+```html
+<cells-bitcoin-dm></cells-bitcoin-dm>
+```
---
+++ cells-bitcoin-dm.js
@@ -0,0 +1,76 @@
+const {html, } = Polymer;
+
+class CellsBitcoinDm extends Polymer.Element {
+
+  static get is() {
+    return 'cells-bitcoin-dm';
+  }
+
+  static get properties() {
+    return {
+      bpiArray: { type: Array, value: [] }
+    };
+  }
+
+  ready() {
+    super.ready();
+    this._getPriceBitcoinList();
+  }
+
+  _getPriceBitcoinList() {
+    let path = 'https://api.coindesk.com/v1/bpi/currentprice.json';
+    this._doRq(path);
+
+  }
+
+  _doRq(endPoint) {
+    this.$.bitcoindp.host = endPoint;
+    this.$.bitcoindp.generateRequest();
+  }
+
+  _rqSuccess(evt) {
+    let data = evt.detail;
+    this.bpiArray.push(data.bpi.USD);
+    this.bpiArray.push(data.bpi.GBP);
+    this.bpiArray.push(data.bpi.EUR);
+    console.table(this.bpiArray);
+  }
+
+  _rqError(err) {
+    console.log(err);
+  }
+
+  static get template() {
+    return html`
+      <cells-generic-dp
+        id = "bitcoindp"
+        method = "GET"
+        on-request-success="_rqSuccess"
+        on-request-error="_rqError">
+      </cells-generic-dp>
+    `;
+  }
+
+  getData() {
+    if (!this.initialLoadHandled) {
+      this._doRqLogin();
+    }
+  }
+
+  _doRqLogin(path, type = 'login-request-start') {
+    this.$.bitcoindp.generateRequest();
+    this.dispatchEvent(new CustomEvent(type));
+  }
+
+  _onRqOk(evt) {
+    const data = evt.detail.results[0];
+    this.dispatchEvent(new CustomEvent('login-request-ok', {detail: data}));
+  }
+
+  _onRqError(err) {
+    this.dispatchEvent(new CustomEvent('login-request-error'));
+  }
+
+}
+
+customElements.define(CellsBitcoinDm.is, CellsBitcoinDm);
