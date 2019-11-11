bf3ab5275c5439c0211dcfb3d8e668d4aa1f31a2
rportocarrero <rportocarrero@gmail.com>
2019-11-11 10:13:55 -0500

    ok

--- cells-bitcoin-dm.js
+++ cells-bitcoin-dm.js
@@ -8,7 +8,9 @@ class CellsBitcoinDm extends Polymer.Element {
 
   static get properties() {
     return {
-      bpiArray: { type: Array, value: [] }
+      bpiArray: { type: Array, value: [] },
+      indBitcoinPrices: {type: Boolean, value: false, notify: true},
+      indBitcoinHistorical: {type: Boolean, value: false, notify: true}
     };
   }
 
@@ -18,9 +20,15 @@ class CellsBitcoinDm extends Polymer.Element {
   }
 
   _getPriceBitcoinList() {
+    this.indBitcoinPrices = true;
     let path = 'https://api.coindesk.com/v1/bpi/currentprice.json';
     this._doRq(path);
+  }
 
+  _getBitcoinHistorical(){
+    this.indBitcoinHistorical = true;
+    let path = 'https://api.coindesk.com/v1/bpi/historical/close.json?start=2019-11-01&end=2019-11-07';
+    this._doRq(path);
   }
 
   _doRq(endPoint) {
@@ -30,36 +38,51 @@ class CellsBitcoinDm extends Polymer.Element {
 
   _rqSuccess(evt) {
     let data = evt.detail;
-    this.bpiArray.push(data.bpi.USD);
-    this.bpiArray.push(data.bpi.GBP);
-    this.bpiArray.push(data.bpi.EUR);
-    console.table(this.bpiArray);
-    // let arreglo = this.bpiArray;
-    let enviar = [];
-    this.bpiArray.forEach(function(item) {
-    let objPadre = new Object();
-    let cells = [];
-    let obj = new Object();
-    obj.label = item.code;
-    obj.isHeader = true;
-    let obj2 = new Object();
-    obj2.label = item.rate;
-    let obj3= new Object();
-    obj3.label = item.description;
-    cells.push(obj);
-    cells.push(obj2);
-    cells.push(obj3);
-    objPadre.cells=cells;
-    enviar.push(objPadre);
-    });
-    console.log(enviar);
-    console.table(enviar);
 
-    this.dispatchEvent(new CustomEvent('bitcoint-price-changed', {
-      composed: true,
-      bubbles: true,
-      detail: enviar
-    }));
+    if(this.indBitcoinPrices){
+      this.bpiArray.push(data.bpi.USD);
+      this.bpiArray.push(data.bpi.GBP);
+      this.bpiArray.push(data.bpi.EUR);
+      console.table(this.bpiArray);
+      // let arreglo = this.bpiArray;
+      let enviar = [];
+      this.bpiArray.forEach(function(item) {
+      let objPadre = new Object();
+      let cells = [];
+      let obj = new Object();
+      obj.label = item.code;
+      obj.isHeader = true;
+      let obj2 = new Object();
+      obj2.label = item.rate;
+      let obj3= new Object();
+      obj3.label = item.description;
+      cells.push(obj);
+      cells.push(obj2);
+      cells.push(obj3);
+      objPadre.cells=cells;
+      enviar.push(objPadre);
+      });
+      console.log(enviar);
+      console.table(enviar);
+  
+      this.dispatchEvent(new CustomEvent('bitcoint-price-changed', {
+        composed: true,
+        bubbles: true,
+        detail: enviar
+      }));
+      this.indBitcoinPrices = false;
+    } else if(this.indBitcoinHistorical){
+      let dataChart = {};
+      dataChart.dates =  Object.keys(data.bpi);
+      dataChart.values = Object.values(data.bpi);
+      this.dispatchEvent(new CustomEvent('bitcoint-price-historical', {detail: dataChart, compesed: true, bubbles: true}));
+      this.indBitcoinHistorical = false;
+    }
+
+ 
+
+
+
   }
 
   _rqError(err) {
