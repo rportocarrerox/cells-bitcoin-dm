e4dc73135e31030a3db5740ccbb8cc7aedc38058
rportocarrero <rportocarrero@gmail.com>
2019-11-10 23:03:37 -0500

    ok

--- cells-bitcoin-dm.js
+++ cells-bitcoin-dm.js
@@ -39,28 +39,22 @@ class CellsBitcoinDm extends Polymer.Element {
     this.bpiArray.forEach(function(item) {
     let objPadre = new Object();
     let cells = [];
-     let obj = new Object();
-     obj.label = item.code;
-     obj.isHeader = true;
-     let obj2 = new Object();
-     obj2.label = item.rate;
+    let obj = new Object();
+    obj.label = item.code;
+    obj.isHeader = true;
+    let obj2 = new Object();
+    obj2.label = item.rate;
     let obj3= new Object();
-     obj3.label = item.description;
-     cells.push(obj);
-     cells.push(obj2);
-     cells.push(obj3);
+    obj3.label = item.description;
+    cells.push(obj);
+    cells.push(obj2);
+    cells.push(obj3);
     objPadre.cells=cells;
-     enviar.push(objPadre);
+    enviar.push(objPadre);
     });
     console.log(enviar);
     console.table(enviar);
-    // cells: [
-    //   {label: 'orange', isHeader: true},
-    //   {label: 77.1},
-    //   {label: 1.8},
-    //   {label: 5.7},
-    //   {label: 95.8}
-    // ]
+
     this.dispatchEvent(new CustomEvent('bitcoint-price-changed', {
       composed: true,
       bubbles: true,
